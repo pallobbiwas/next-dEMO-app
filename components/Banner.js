92e59682f43942433f29939eab747b17fdb46251
pallobbiswas <pallobbiswas326@gmail.com>
2022-07-14 09:41:42 -0400

    start

---
+++ components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>This is Banner</div>
+  )
+}
+
+export default Banner
---
+++ components/OurTeam.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OurTeam() {
+  return (
+    <div>This is OurTeam</div>
+  )
+}
+
+export default OurTeam
