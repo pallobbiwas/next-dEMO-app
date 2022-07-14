59c07120e107ccd28bba8bda678f002fc61d8b4d
pallobbiswas <pallobbiswas326@gmail.com>
2022-07-14 11:31:03 -0400

    home done

--- components/Homes.js
+++ components/Homes.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>This is Home</div>
-  )
+    <div className="bg-pink-100">
+      <section>
+        <div className="px-72 py-60 text-center">
+          <h1 className="text-5xl font-bold mb-3">Lorem ipsum dolor lprea,3
+           sit amet.</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit amet
+            magni nobis vero eum ut temporibus, molestias aspernatur vel
+            sapiente?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam laudantium dolorum quod adipisci magni nobis, aliquam quasi vitae soluta minus?
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
